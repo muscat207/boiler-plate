2ca7608b0fcbb2f5c4e50cc31e22497888c861c7
muscat207 <hjs90561@gmail.com>
2022-05-07 17:10:23 +0900

    저장소에 올림

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://muscat:aktmzkxm207@devconnecter.a8zjb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('Hello World! 1234')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
